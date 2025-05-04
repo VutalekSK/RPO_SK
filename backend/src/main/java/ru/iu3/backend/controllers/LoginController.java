352675324dbb2a79b4ff326e8389f5a14cf1054d
= <mr.sergei02@mail.ru>
2025-05-04 10:36:44 +0300

    LAB9

--- backend/src/main/java/ru/iu3/backend/controllers/LoginController.java
+++ backend/src/main/java/ru/iu3/backend/controllers/LoginController.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import ru.iu3.backend.models.User;
 import ru.iu3.backend.repositories.UserRepository;
 import ru.iu3.backend.tools.Utils;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
